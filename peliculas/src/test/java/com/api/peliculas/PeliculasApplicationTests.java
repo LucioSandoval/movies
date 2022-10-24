b0cd2a5e3335249632ee88c601920d7ca74115d9
LucioSandoval <lucio.scaceres@gmail.com>
2022-10-24 14:48:55 -0300

    subiendo proyecto maven

---
+++ peliculas/src/test/java/com/api/peliculas/PeliculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.peliculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeliculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
